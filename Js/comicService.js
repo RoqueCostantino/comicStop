928cbc60e4c84e323da0636973630642f42cf435
nnombarasco <nicolas.nombarasco@gmail.com>
2018-09-12 19:41:22 -0300

    fix function
    arreglos sobre check-in, search

---
+++ Js/comicService.js
@@ -0,0 +1,345 @@
+let comics = [
+    {
+        "_id": "5b8eec8bd67a0fa0e563402d",
+        "index": 0,
+        "name": "Bonita Hays",
+        "picture": "http://placehold.it/32x32",
+        "age": 1992,
+        "pages": 113,
+        "author": "Suarez Buchanan",
+        "company": "ISOSTREAM",
+        "about": "Do dolor tempor veniam ullamco. Eu irure magna labore sit sit Lorem nulla. Incididunt elit cupidatat non ullamco excepteur proident est elit consectetur. Deserunt magna adipisicing commodo consectetur ullamco do aliquip. Fugiat cupidatat elit enim tempor.\r\n",
+        "tags": [
+            "velit",
+            "sunt",
+            "laboris",
+            "sunt",
+            "laboris",
+            "cupidatat",
+            "qui"
+        ]
+    },
+    {
+        "_id": "5b8eec8b3d47751371d1c2c1",
+        "index": 1,
+        "name": "Joann Knight",
+        "picture": "http://placehold.it/32x32",
+        "age": 2007,
+        "pages": 165,
+        "author": "Elva Lyons",
+        "company": "NAMEBOX",
+        "about": "Minim qui mollit eu cillum irure qui consectetur eiusmod aliquip sit minim dolore cupidatat anim. Enim laboris sint quis incididunt consectetur sunt ut aute eiusmod voluptate pariatur ullamco mollit laboris. Culpa consequat laborum est consequat adipisicing do exercitation in aute in sunt. Ullamco nisi incididunt magna sit incididunt laborum.\r\n",
+        "tags": [
+            "ea",
+            "nisi",
+            "enim",
+            "incididunt",
+            "non",
+            "cillum",
+            "culpa"
+        ]
+    },
+    {
+        "_id": "5b8eec8b9896fba777425c9f",
+        "index": 2,
+        "name": "Kathryn Macias",
+        "picture": "http://placehold.it/32x32",
+        "age": 2001,
+        "pages": 159,
+        "author": "Rosemarie Hendrix",
+        "company": "LUNCHPAD",
+        "about": "Veniam est in culpa nulla sit do. Mollit est minim aliquip fugiat non fugiat irure ipsum anim duis proident. Fugiat qui commodo cillum fugiat mollit proident nostrud labore.\r\n",
+        "tags": [
+            "aute",
+            "incididunt",
+            "consequat",
+            "ut",
+            "irure",
+            "ipsum",
+            "mollit"
+        ]
+    },
+    {
+        "_id": "5b8eec8b2b122c13c66b8bae",
+        "index": 3,
+        "name": "Sybil Osborn",
+        "picture": "http://placehold.it/32x32",
+        "age": 1978,
+        "pages": 115,
+        "author": "Dixie Murphy",
+        "company": "FANFARE",
+        "about": "Ipsum ex sit eu enim laborum qui Lorem et ullamco voluptate esse. Esse id irure est irure magna laboris. Nulla officia dolor incididunt non commodo. Nisi occaecat ipsum deserunt pariatur et. Incididunt incididunt consectetur proident ullamco tempor duis dolore. Sunt sit et amet pariatur ullamco velit laboris.\r\n",
+        "tags": [
+            "aliquip",
+            "sint",
+            "elit",
+            "aliqua",
+            "id",
+            "tempor",
+            "mollit"
+        ]
+    },
+    {
+        "_id": "5b8eec8b60805735ec56ed09",
+        "index": 4,
+        "name": "Bruce Hahn",
+        "picture": "http://placehold.it/32x32",
+        "age": 1956,
+        "pages": 245,
+        "author": "Donaldson Parsons",
+        "company": "HALAP",
+        "about": "Consequat reprehenderit tempor est est cupidatat commodo non dolore minim laboris voluptate elit. Mollit consectetur dolore voluptate deserunt sint commodo sit enim amet ex incididunt est laboris exercitation. Laborum aliqua eiusmod Lorem ullamco duis deserunt labore et duis sunt.\r\n",
+        "tags": [
+            "aute",
+            "magna",
+            "excepteur",
+            "sint",
+            "enim",
+            "esse",
+            "commodo"
+        ]
+    },
+    {
+        "_id": "5b8eec8b3e9dd88b507b3fd7",
+        "index": 5,
+        "name": "Ramos Sanders",
+        "picture": "http://placehold.it/32x32",
+        "age": 2012,
+        "pages": 425,
+        "author": "Schultz Campbell",
+        "company": "MUSAPHICS",
+        "about": "Lorem nostrud culpa consequat irure voluptate anim proident irure aliqua fugiat aliquip ex cupidatat velit. Id esse aliquip adipisicing irure laboris occaecat pariatur aliqua commodo do. Consequat elit cupidatat amet do elit ex adipisicing adipisicing occaecat nisi non. Proident aliquip adipisicing cillum cillum consequat consectetur voluptate aute reprehenderit. Proident ad enim laborum cupidatat pariatur qui laboris nisi non reprehenderit exercitation occaecat.\r\n",
+        "tags": [
+            "consequat",
+            "aliqua",
+            "tempor",
+            "pariatur",
+            "labore",
+            "sit",
+            "occaecat"
+        ]
+    },
+    {
+        "_id": "5b8eec8b168661147be855f0",
+        "index": 6,
+        "name": "Kelly Hester",
+        "picture": "http://placehold.it/32x32",
+        "age": 1994,
+        "pages": 441,
+        "author": "Lauren Walters",
+        "company": "SKYPLEX",
+        "about": "Excepteur nulla ipsum quis sint aute minim quis occaecat non occaecat. Velit commodo sunt dolor aliqua et tempor nisi nulla duis. Id aute nostrud ullamco pariatur do ad sunt ex ipsum magna fugiat ea. Lorem et aliqua exercitation aute magna voluptate aute magna non Lorem. Magna eiusmod pariatur enim enim laboris aute magna consectetur est mollit. Sunt duis tempor enim nostrud.\r\n",
+        "tags": [
+            "est",
+            "sunt",
+            "ut",
+            "irure",
+            "exercitation",
+            "mollit",
+            "nisi"
+        ]
+    },
+    {
+        "_id": "5b8eec8bf0266d8c8ab1f5b5",
+        "index": 7,
+        "name": "Sophie Leon",
+        "picture": "http://placehold.it/32x32",
+        "age": 1950,
+        "pages": 297,
+        "author": "Jewell Hopkins",
+        "company": "ZIORE",
+        "about": "Anim eu laborum anim voluptate aliquip incididunt amet. Laboris irure anim officia Lorem consequat commodo voluptate culpa consequat. Labore voluptate irure consectetur proident non magna. Elit pariatur eiusmod ad laboris pariatur consequat nisi.\r\n",
+        "tags": [
+            "tempor",
+            "laborum",
+            "fugiat",
+            "in",
+            "exercitation",
+            "culpa",
+            "enim"
+        ]
+    },
+    {
+        "_id": "5b8eec8bfdcc5cbd09c3e10e",
+        "index": 8,
+        "name": "Natasha England",
+        "picture": "http://placehold.it/32x32",
+        "age": 1975,
+        "pages": 458,
+        "author": "Kerr Ashley",
+        "company": "ZILLA",
+        "about": "In ex exercitation irure ullamco dolore adipisicing culpa excepteur commodo quis veniam velit do. Laborum id duis aliquip eu esse ea. Laboris officia esse esse aliquip laborum qui excepteur non est anim exercitation. Cillum irure ad exercitation laborum nulla amet exercitation. Reprehenderit non non qui aliqua elit pariatur.\r\n",
+        "tags": [
+            "aliquip",
+            "enim",
+            "cupidatat",
+            "sunt",
+            "Lorem",
+            "et",
+            "consequat"
+        ]
+    },
+    {
+        "_id": "5b8eec8bebe491ecbdb3acfa",
+        "index": 9,
+        "name": "Gail Sharp",
+        "picture": "http://placehold.it/32x32",
+        "age": 1964,
+        "pages": 340,
+        "author": "Whitehead Alexander",
+        "company": "NURPLEX",
+        "about": "Laborum magna sunt officia cillum eiusmod et laborum anim voluptate. Non do aute nisi amet sint. Ullamco veniam cillum non aliquip excepteur duis proident non et. Magna aliquip fugiat fugiat pariatur. Mollit aliqua ex pariatur qui nisi aliqua reprehenderit laborum commodo est do.\r\n",
+        "tags": [
+            "proident",
+            "non",
+            "tempor",
+            "reprehenderit",
+            "ad",
+            "aliquip",
+            "elit"
+        ]
+    },
+    {
+        "_id": "5b8eec8b627c9498387fb578",
+        "index": 10,
+        "name": "Terry Willis",
+        "picture": "http://placehold.it/32x32",
+        "age": 1983,
+        "pages": 162,
+        "author": "Michele Castaneda",
+        "company": "TURNABOUT",
+        "about": "Occaecat culpa duis incididunt aliqua. Aliqua dolor nostrud aliquip mollit cillum excepteur nostrud ipsum Lorem ex velit. Quis reprehenderit duis sint Lorem ex veniam ut exercitation amet incididunt Lorem tempor occaecat. Excepteur veniam ipsum est reprehenderit. Exercitation mollit aute excepteur do labore qui ut. Fugiat ullamco labore nulla nostrud nisi qui do consectetur est incididunt. Reprehenderit id proident reprehenderit eiusmod laborum dolor voluptate qui in veniam amet excepteur sunt id.\r\n",
+        "tags": [
+            "eiusmod",
+            "eiusmod",
+            "nostrud",
+            "exercitation",
+            "adipisicing",
+            "fugiat",
+            "veniam"
+        ]
+    },
+    {
+        "_id": "5b8eec8b76226d1476923c86",
+        "index": 11,
+        "name": "Riley Conley",
+        "picture": "http://placehold.it/32x32",
+        "age": 1974,
+        "pages": 334,
+        "author": "Dawson Rodgers",
+        "company": "CUBIX",
+        "about": "Velit excepteur aute magna dolor adipisicing sit duis. Velit consequat elit reprehenderit dolore ipsum adipisicing esse sint. Ex et duis qui ullamco reprehenderit eu minim officia reprehenderit adipisicing. Cillum qui minim irure sunt irure Lorem. Aliquip eiusmod mollit labore labore exercitation tempor cillum eu consequat nisi ea. Ea laboris laboris dolore aute et officia elit in excepteur veniam proident sit commodo in.\r\n",
+        "tags": [
+            "veniam",
+            "sint",
+            "occaecat",
+            "elit",
+            "minim",
+            "velit",
+            "cupidatat"
+        ]
+    },
+    {
+        "_id": "5b8eec8b6ccd24d59271e582",
+        "index": 12,
+        "name": "Judith Hull",
+        "picture": "http://placehold.it/32x32",
+        "age": 1992,
+        "pages": 384,
+        "author": "Kitty Vinson",
+        "company": "GEEKY",
+        "about": "Nulla nisi aute non tempor ullamco anim commodo exercitation elit pariatur proident tempor. Dolor enim incididunt exercitation ea voluptate duis. Laborum labore ea veniam ipsum consectetur occaecat est. Sunt ut non elit nisi ipsum est veniam magna qui ipsum ad. Anim est dolor non duis nisi tempor adipisicing pariatur. Culpa sint adipisicing consequat laborum magna deserunt ad fugiat. Fugiat do pariatur velit sunt enim eu eiusmod mollit aliqua.\r\n",
+        "tags": [
+            "elit",
+            "do",
+            "ut",
+            "magna",
+            "elit",
+            "enim",
+            "officia"
+        ]
+    },
+    {
+        "_id": "5b8eec8b70d7fabcc83f24cb",
+        "index": 13,
+        "name": "Carney Knox",
+        "picture": "http://placehold.it/32x32",
+        "age": 1981,
+        "pages": 322,
+        "author": "Smith Morgan",
+        "company": "DOGNOSIS",
+        "about": "Enim voluptate ex anim nulla cillum ad. Velit pariatur ut nostrud aliquip. Eu ex aute consectetur ipsum labore sit. Irure pariatur amet ut elit et deserunt magna in sunt proident mollit. Ex culpa consequat excepteur est ipsum aliqua aute et cupidatat quis eu elit.\r\n",
+        "tags": [
+            "quis",
+            "sit",
+            "dolor",
+            "quis",
+            "culpa",
+            "Lorem",
+            "magna"
+        ]
+    },
+    {
+        "_id": "5b8eec8b9f879b6c81d525fd",
+        "index": 14,
+        "name": "Carter Bryan",
+        "picture": "http://placehold.it/32x32",
+        "age": 1954,
+        "pages": 188,
+        "author": "Jolene Cortez",
+        "company": "DAIDO",
+        "about": "Anim duis aliqua ex cillum non laboris et commodo et. Enim sit mollit incididunt sit ea duis veniam mollit qui. Sint tempor commodo ullamco ea voluptate. Occaecat dolor Lorem minim reprehenderit enim irure. Dolore reprehenderit officia irure dolor adipisicing nulla excepteur incididunt ad amet.\r\n",
+        "tags": [
+            "nulla",
+            "minim",
+            "reprehenderit",
+            "officia",
+            "tempor",
+            "laborum",
+            "qui"
+        ]
+    }]
+
+
+
+    /*check-in*/
+function check(num) {
+    let found = false;
+
+        comics.forEach(function (comic) {
+            if (comic._id === num) {
+                found = true;
+            } 
+        });
+        return found;
+    }
+
+        /*funcion que devuelve todos los datos del comic*/
+    function allInfo() {
+        for (let a = 0; a < comics.length; a++) {
+            let datos = comics[a];
+
+            return ' ID: ' + datos._id + ' Index: ' + datos.index + ' Name: ' + datos.name + ' Author: ' + datos.author + ' Age: ' + datos.age + ' Company: ' + datos.company + ' About: ' + datos.about;
+        }
+    }
+
+    /*funcion que devuelve solo datos utiles*/
+    function basicInfo() {
+        for (let a = 0; a < comics.length; a++) {
+            let datos = comics[a];
+
+            return 'Name: ' + datos.name + ' Author: ' + datos.author + ' Age: ' + datos.age;
+        }
+    }
+
+    /*search*/
+    function search(srt) {
+        let arrayComics = [];
+
+        comics.forEach(function (comic){
+            if (comic.name.indexOf(srt) != -1) {
+                arrayComics = comics.push(comic);
+            }
+        })
+        return arrayComics;
+    }
