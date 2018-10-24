f51dd9b2522aef4b4dbe2736d9b6fff28097e04a
Roque <38595015+RoqueCostantino@users.noreply.github.com>
2018-10-23 19:38:02 -0500

    arreglos

---
+++ comic.js
@@ -0,0 +1,9 @@
+/*var pu=0, tot=0, resultVote=0;
+
+function VoteBar(comic1,comic2){
+        tot=comic1+comic2;                               
+        pu=(comic1 * 100)/tot;
+        tot= 100%;
+        resultVote = tot-pu;
+};               
+
